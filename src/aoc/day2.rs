d3d9806d62212e965f142364d34b9d6d7ec340bf
Daniel Vieites <dvieitest@gmail.com>
2024-12-02 23:16:12 +0100

    day 2, part 2

--- src/aoc/day2.rs
+++ src/aoc/day2.rs
@@ -14,38 +14,101 @@ enum DIR {
 }
 
 impl Day2 {
-    fn is_safe(report: &[i64]) -> bool {
+    fn is_safe_part1(report: &[i64]) -> bool {
         if report.len() < 2 {
-            return false
+            return false;
         }
 
         let mut dir: Option<DIR> = None;
 
         for pair in report.windows(2) {
-            let (current, next) = (pair[0], pair[1]);
-            let diff = next - current;
-            let abs_diff = diff.abs();
-
-            if abs_diff == 0 || abs_diff > 3 {
+            if !Day2::is_valid_pair(&mut dir, pair[0], pair[1]) {
                 return false;
-            }
-
-            let current_dir = if diff > 0 {
-                DIR::DECREASING
-            } else {
-                DIR::INCREASING
-            };
-
-            if let Some(prev_dir) = &dir {
-                if prev_dir != &current_dir {
-                    return false;
-                }
-            } else {
-                dir = Some(current_dir);
             }
         }
 
         true
+    }
+
+    fn get_dir(current: i64, next: i64) -> DIR {
+        if current - next < 0 {
+            DIR::INCREASING
+        } else {
+            DIR::DECREASING
+        }
+    }
+
+    fn check_diff(current: i64, next: i64) -> bool {
+        let diff = next - current;
+        let abs_diff = diff.abs();
+
+        if abs_diff == 0 || abs_diff > 3 {
+            return false;
+        }
+
+        true
+    }
+
+    fn is_valid_pair(dir: &mut Option<DIR>, current: i64, next: i64) -> bool {
+        if !Day2::check_diff(current, next) {
+            return false;
+        }
+
+        let current_dir = Day2::get_dir(current, next);
+
+        if let Some(prev_dir) = &dir {
+            if prev_dir != &current_dir {
+                return false;
+            }
+        } else {
+            *dir = Some(current_dir);
+        }
+
+        true
+    }
+
+    fn is_valid_report(report: &[i64]) -> (bool, usize) {
+        if report.len() < 2 {
+            return (false, 0);
+        }
+
+        let mut dir: Option<DIR> = None;
+
+        for (i, pair) in report.windows(2).enumerate() {
+            if !Day2::is_valid_pair(&mut dir, pair[0], pair[1]) {
+                return (false, i);
+            }
+        }
+
+        (true, 0)
+    }
+
+    fn remove_index<T: Copy>(slice: &[T], index: usize) -> Vec<T> {
+        let mut result = Vec::with_capacity(slice.len() - 1);
+        result.extend_from_slice(&slice[..index]);
+        result.extend_from_slice(&slice[index + 1..]);
+        result
+    }
+
+    fn is_safe_part2(r: &[i64]) -> bool {
+        let (valid, i) = Day2::is_valid_report(r);
+        if valid {
+            return true;
+        }
+
+        if i == 1 && Day2::is_valid_report(&r[1..]).0 {
+            return true;
+        }
+
+        if Day2::is_valid_report(&Day2::remove_index(r, i)).0 {
+            return true;
+        }
+
+        if i + 1 < r.len() && Day2::is_valid_report(&Day2::remove_index(r, i + 1)).0 {
+            return true;
+        }
+
+        false
     }
 }
 
@@ -72,15 +135,12 @@ impl Solution for Day2 {
 
     fn part1() -> u64 {
         let reports: Vec<Report> = Day2::parse_input("./input/day2").unwrap().collect();
-        reports
-            .iter()
-            .map(|r| Day2::is_safe(&r))
-            .filter(|&v| v)
-            .count() as u64
+        reports.iter().filter(|&r| Day2::is_safe_part1(r)).count() as u64
     }
 
     fn part2() -> u64 {
-        0
+        let reports: Vec<Report> = Day2::parse_input("./input/day2").unwrap().collect();
+        reports.iter().filter(|&r| Day2::is_safe_part2(r)).count() as u64
     }
 }
 
@@ -90,21 +150,43 @@ mod test {
 
     #[test]
     fn test_day2_is_safe() {
-        // 7 6 4 2 1
         let report: Report = vec![7, 6, 4, 2, 1];
-        assert!(Day2::is_safe(&report));
+        assert!(Day2::is_safe_part1(&report));
     }
 
     #[test]
     fn test_day2_is_not_safe() {
         let report: Report = vec![1, 2, 7, 8, 9];
-        assert!(!Day2::is_safe(&report));
+        assert!(!Day2::is_safe_part1(&report));
     }
 
     #[test]
     fn test_day2_part1_example() {
         let reports: Vec<Report> = Day2::parse_input("./example/day2").unwrap().collect();
-        let count: usize = reports.iter().map(|r| Day2::is_safe(r)).filter(|&v| v).count();
+        let count: usize = reports
+            .iter()
+            .map(|r| Day2::is_safe_part1(r))
+            .filter(|&v| v)
+            .count();
         assert_eq!(count, 2);
+    }
+
+    #[test]
+    fn test_day2_is_safe_part2() {
+        let r: Report = vec![24, 20, 25, 26];
+        assert!(Day2::is_safe_part2(&r));
+    }
+
+    #[test]
+    fn test_day2_is_not_safe_part2() {
+        let r: Report = vec![65, 68, 71, 71, 72, 79];
+        assert!(!Day2::is_safe_part2(&r));
+    }
+
+    #[test]
+    fn test_day2_part2_example() {
+        let reports: Vec<Report> = Day2::parse_input("./example/day2").unwrap().collect();
+        let count = reports.iter().filter(|&r| Day2::is_safe_part2(r)).count() as u64;
+        assert_eq!(count, 4);
     }
 }
